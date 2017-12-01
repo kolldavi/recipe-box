f7630a80ecdee3b511acb7b2475ff55ab4405713
kolldavi <kolldavi@gmail.com>
2017-12-01 14:34:08 -0600

    added delete method

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './css/src/css/styles.css';
 
 class Recipe extends Component {
+  state = {
+    isCollapsed: true
+  };
+
+  toggleClass = () => {
+    this.setState({ isCollapsed: !this.state.isCollapsed });
+  };
   render() {
-    const { title, list, displayed } = this.props;
+    const { title, ingredients, removeRecipe } = this.props;
+    console.log(removeRecipe);
+
+    const { isCollapsed } = this.state;
     return (
-      <div className="Main">
-        <p>{title}</p>
-        <ol>{list.map(item => <li>{item}</li>)}</ol>
+      <div className="container">
+        <div className="accordion" onClick={this.toggleClass}>
+          <h3>{title}</h3>
+        </div>
+        <div className={isCollapsed ? 'hide' : 'expand'}>
+          <h4 className="ingredients">Ingredients</h4>
+          <hr />
+          <ol>
+            {ingredients.map((item, value) => <li key={value}>{item}</li>)}
+          </ol>
+          <button className="btn btnDelete" onClick={() => removeRecipe(title)}>
+            delete
+          </button>
+          <button className="btn btnEdit">edit</button>
+        </div>
       </div>
     );
   }
@@ -16,7 +38,12 @@ class Recipe extends Component {
 
 class App extends Component {
   state = {
-    recipeList: [{ title: 'added', list: [1, 2] }]
+    recipeList: [
+      {
+        title: 'Dinner',
+        ingredients: ['chicken', 'broccoli']
+      }
+    ]
   };
   addRecipe = recipe => {
     let newList = this.state.recipeList.concat(recipe);
@@ -25,29 +52,44 @@ class App extends Component {
     });
     console.log(this.state);
   };
-  componentDidMount() {
-    console.log(this.state);
-  }
+  removeRecipe = title => {
+    let newList = this.state.recipeList.filter(
+      recipe => recipe.title !== title
+    );
 
+    this.setState({ recipeList: newList });
+  };
+  ComponentDidUpdate() {
+    console.log('update', this.state);
+  }
   render() {
     const { recipeList } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Recipe Box</h1>
+          <h1 className="heading-primary">Recipe Box</h1>
         </header>
-        {recipeList.map(recipe => (
-          <Recipe
-            key={recipe.title}
-            title={recipe.title}
-            list={recipe.list}
-            displayed={false}
-          />
-        ))}
-
-        <button onClick={() => this.addRecipe({ title: 'new', list: [5, 6] })}>
-          addRecipe
-        </button>
+        <div className="list-container">
+          <button
+            className="btn"
+            onClick={() =>
+              this.addRecipe({
+                title: 'Breakfast',
+                ingredients: ['bacon', 'eggs', 'ham']
+              })
+            }>
+            add new recipe
+          </button>
+          {recipeList.map(recipe => (
+            <Recipe
+              className="accordionItem"
+              key={recipe.title}
+              title={recipe.title}
+              ingredients={recipe.ingredients}
+              removeRecipe={this.removeRecipe}
+            />
+          ))}
+        </div>
       </div>
     );
   }
